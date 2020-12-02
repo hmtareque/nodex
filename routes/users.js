0d3a3ea27e5414a8b3b8242e3a9827880b457584
Hasan Tareque <hmtareque@gmail.com>
2020-12-02 02:36:49 +0000

    nodemon added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/123', function(req, res, next) {
+  res.send('specified user');
+});
+
+
 
 module.exports = router;
